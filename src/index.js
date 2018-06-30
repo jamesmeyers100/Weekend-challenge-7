d7f775b7038c3b83db27cb6e1f7c084154e31da6
jamesmeyers88 <jamesmeyers88@gmail.com>
2018-06-30 11:57:53 -0500

    added reduxStore/reducer to index

--- src/index.js
+++ src/index.js
@@ -3,6 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+//reduxStore Stuff
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import logger from 'redux-logger';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const feedbackReducer = (state={}, action) => {
+    if( action.type === 'ADD_FEELING'){
+        console.log(`Hey, I'm reducing your feeling!`, action);
+        return [...state, action.payload];
+    } else if( action.type === 'ADD_SUPPORTED') {
+        console.log(`Hey, I'm reducing your supported!`, action)
+        return [...state, action.payload];
+    } else if( action.type === 'ADD_COMMENT') {
+        console.log(`Hey, I'm reducint your comment`, action)
+        return [...state, action.payload];
+    } 
+}
+
+const storeInstance = createStore(
+    combineReducers({
+        feedbackReducer
+    }),
+    applyMiddleware(logger)
+);
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
